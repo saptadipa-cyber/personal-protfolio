ff27dfd11124de09a8740438e8098bcd6d2061e0
saptadipa-cyber <saptateri342@gmail.com>
2024-07-28 21:03:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var typed= new typed("text",{
+    strings: ["Networking" , "Cloud Computing"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backdelay: 1000,
+    loop: true
+});
+
+
+<script>
+    var typed = new Typed('#element', {
+    strings: ['Cloud Computing', 'Networking'],
+    typeSpeed: 50,
+    });
+  </script>
